58835a094d91a70989a5ba588346176a42cdec3b
Jukiyoomi <jnguessan2003@gmail.com>
2022-11-07 17:55:48 +0100

    user routes, user update/delete

--- src/middlewares/controllers/user.controllers.ts
+++ src/middlewares/controllers/user.controllers.ts
@@ -1,9 +1,9 @@
 import {Request, Response} from "express";
 import {User} from "~/middlewares/models/user.model";
 
-export const getMe = async(req: Request, res: Response) => {
+export const getUser = async(req: Request, res: Response) => {
 	console.log(req.user)
-	const currentUser = await User.findById(req.user)
+	const currentUser = await User.findById(req.user).select('-password')
 	if(!currentUser) return res.status(404).json({type: "error", data: {error: "No user found"}})
 
 	if(req.tokens) {
@@ -11,5 +11,29 @@ export const getMe = async(req: Request, res: Response) => {
 		req.tokens = undefined
 		return res.json({type: "success", data: {user: currentUser, tokens}})
 	}
-	res.json({type: "success", data: {user: currentUser}})
+	res.status(200).json({type: "success", data: {user: currentUser}})
+}
+
+export const updateUser = async(req: Request, res: Response) => {
+	try {
+		const currentUser = await User.findByIdAndUpdate(req.user, req.body, {
+			upsert: true,
+			new: true
+		}).select('-password')
+		res.status(200).json({type: "success", data: {user: currentUser}})
+
+	} catch (err) {
+		return res.status(500).json({type: "error", data: {error: err}});
+	}
+}
+
+export const deleteUser = async(req: Request, res: Response) => {
+	try {
+		const currentUser = await User.findByIdAndDelete(req.user)
+		if(currentUser) {
+			res.status(204).json({})
+		}
+	} catch (err) {
+		return res.status(500).json({type: "error", data: {error: err}});
+	}
 }
--- src/middlewares/routes/user.routes.ts
+++ src/middlewares/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import {getMe} from "~/middlewares/controllers/user.controllers";
+import {deleteUser, getUser, updateUser} from "~/middlewares/controllers/user.controllers";
 import {verifyTokens} from "~/services/jwt.services";
 
 const userRoutes = Router()
 
-userRoutes.get("/", verifyTokens, getMe)
+userRoutes.get("/", verifyTokens, getUser)
+userRoutes.put("/", verifyTokens, updateUser)
+userRoutes.delete("/", verifyTokens, deleteUser)
 
 export default userRoutes
